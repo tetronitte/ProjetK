90e40451bff1adcdbb3553df5bdf8f00398bba0c
tetronitte <axel.ndc@gmail.com>
2021-02-02 16:09:39 +0100

    suppression de fichier js

--- assets/php/logout.php
+++ assets/php/logout.php
@@ -7,10 +7,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     session_destroy();
 
     // Suppression des cookies de connexion automatique
-    //if($_POST['noAutolog'][0] == 'on') {    
+    if($_POST['noAutolog'][0] == 'on') {    
         setcookie('nickname','');
         setcookie('pwd','');
-    //}
+    }
 }
 header('Location: http://php.projetk/');
 exit();
