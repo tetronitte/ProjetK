822c1466c281cabbd66a4c86805742ea3b1bd6ab
tetronitte <axel.ndc@gmail.com>
2021-02-02 14:25:16 +0100

    asset en assets

--- index.php
+++ index.php
@@ -1,18 +1,34 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>ProjetK</title>
-        <meta charset="UTF-8" />
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-        <link rel="stylesheet" href="asset/css/index.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body id="body">
-        <div class="container-fluid">
-        </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-        <script src="asset/js/login.js"></script>
-        <script src="asset/js/signin.js"></script>
-    </body>
-</html>
+<span>INSCRIPTION</span><br/>
+<form action="signin.php" method="post">
+    <div><label for="nickname">Pseudo</label><input type="text" name="nickname"/></div>
+    <div><label for="password">Mot de passe</label><input type="text" name="password"/></div>
+    <div><label for="vPassword">Vérification de mot de passe</label><input type="text" name="vPassword"/></div>
+    <button type="submit">signin</button>
+</form>
+
+<?php
+session_start();
+var_dump($_COOKIE);
+if(isset($_COOKIE['nickname']) && isset($_COOKIE['pwd'])) {
+    echo '<p>Vous êtes connecté grâce aux cookies</p>';
+}
+else if(isset($_SESSION['nickname']) && isset($_SESSION['id'])) {
+    echo '<p>Vous êtes connecté</p>';
+}
+else {
+    echo '<br/>
+    <span>CONNEXION</span><br/>
+    <form action="login.php" method="post">
+        <div><label for="nickname">Pseudo</label><input type="text" name="nickname"/></div>
+        <div><label for="password">Mot de passe</label><input type="text" name="password"/></div>
+        <div><label for="autolog">Connexion automatique</label><input type="checkbox" id="autolog" name="autolog[]"/></div>
+        <button type="submit">login</button>
+    </form>';
+}
+?>
+<form action="logout.php" method="post">
+    <button type="submit">logout</button>
+</form>
+
+
+
